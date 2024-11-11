08d2b47df7b769a1afc3cd7ae2c0191905b14b2b
jaikirat2909 <124550982+jaikirat2909@users.noreply.github.com>
2024-11-11 14:16:26 +0530

    Token and schema model in backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,73 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from the backend!');
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb+srv://harmeet96:U75SYB05AWP1@cluster0.0elap.mongodb.net/e-commerce");
+
+//Image Storage Engine 
+const storage = multer.diskStorage({
+  destination: './upload/images',
+  filename: (req, file, cb) => {
+    return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+  }
+})
+const upload = multer({ storage: storage })
+app.post("/upload", upload.single('product'), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `/images/${req.file.filename}`
+  })
+})
+
+// Route for Images folder
+app.use('/images', express.static('upload/images'));
+
+// MiddleWare to fetch user from token
+const fetchuser = async (req, res, next) => {
+  const token = req.header("auth-token");
+  if (!token) {
+    res.status(401).send({ errors: "Please authenticate using a valid token" });
+  }
+  try {
+    const data = jwt.verify(token, "secret_ecom");
+    req.user = data.user;
+    next();
+  } catch (error) {
+    res.status(401).send({ errors: "Please authenticate using a valid token" });
+  }
+};
+
+// Schema for creating user model
+const Users = mongoose.model("Users", {
+  name: { type: String },
+  email: { type: String, unique: true },
+  password: { type: String },
+  cartData: { type: Object },
+  date: { type: Date, default: Date.now() },
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+// Schema for creating Product
+const Product = mongoose.model("Product", {
+  id: { type: Number, required: true },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  category: { type: String, required: true },
+  new_price: { type: Number },
+  old_price: { type: Number },
+  date: { type: Date, default: Date.now },
+  avilable: { type: Boolean, default: true },
+});
+
+// ROOT API Route For Testing
+app.get("/", (req, res) => {
+  res.send("Root");
 });
