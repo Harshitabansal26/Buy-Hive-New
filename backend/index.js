4fb67403d868e033e9bff680a6414f07ed2b1101
Harshita <harshitabansal3142@gmail.com>
2024-11-18 15:14:32 +0530

    signup

--- backend/index.js
+++ backend/index.js
@@ -99,3 +99,34 @@ app.post('/login', async (req, res) => {
   }
 })
 
+
+//Create an endpoint at ip/auth for regestring the user & sending auth-token
+app.post('/signup', async (req, res) => {
+  console.log("Sign Up");
+  let success = false;
+  let check = await Users.findOne({ email: req.body.email });
+  if (check) {
+    return res.status(400).json({ success: success, errors: "existing user found with this email" });
+  }
+  let cart = {};
+  for (let i = 0; i < 300; i++) {
+    cart[i] = 0;
+  }
+  const user = new Users({
+    name: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    cartData: cart,
+  });
+  await user.save();
+  const data = {
+    user: {
+      id: user.id
+    }
+  }
+
+  const token = jwt.sign(data, 'secret_ecom');
+  success = true;
+  res.json({ success, token })
+})
+
